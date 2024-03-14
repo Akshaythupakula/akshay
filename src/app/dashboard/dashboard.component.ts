e220e85cfb8f17a53046194a25a27e6aa2ce49d1
Akshaythupakula <akshaygoud509@gmail.com>
2024-03-14 18:15:35 +0530

    mobile

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,11 +7,44 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
-  para=false
+  para='0'
+none=''
+mobile1=false
+expand1=false
 
-paragraph(){
-  this.para=true
+isNavbarCollapsed = false;
+
+toggleNavbar() {
+  this.isNavbarCollapsed = !this.isNavbarCollapsed;
+}
+
+nav(){
+  this.isNavbarCollapsed = false;
+}
+paragraph(eventType: string){
+  if(eventType=='1')
+  {
+    this.para='1'
+    this.none='none'
+  }else if(eventType=='2'){
+    this.para='2'
+    
+  }
+  else{
+    this.para='3'
+  }
+  
+
+}
+mobile(){
+  this.mobile1=true
+}
+expanded(){
+this.expand1=true
+}
+
+
 
 }
 
-}
+
